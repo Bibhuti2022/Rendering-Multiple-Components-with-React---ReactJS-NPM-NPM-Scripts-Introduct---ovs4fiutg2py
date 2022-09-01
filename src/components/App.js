1a39693c60bd9a788180ffd9f64b737974b55dd2
Bibhuti2022 <102380026+Bibhuti2022@users.noreply.github.com>
2022-09-01 09:24:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1>Online Educator</h1>
+      <p>This is an online tutorial for edcation</p>
+    </div>
   )
 }
 
